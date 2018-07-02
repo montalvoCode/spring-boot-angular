2d5a0624565e6cae0c11c8a2c4d481c1001c1110
montalvoCode <montalvocode1997@hotmail.com>
2018-07-01 23:40:24 -0500

    Service Crud

--- client/src/app/shared_service/user.service.ts
+++ client/src/app/shared_service/user.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 import { Users } from '../users';
 
 @Injectable()
@@ -11,32 +9,28 @@ export class UserService {
   private baseUrl: string = 'http://localhost:8080/';
   private headers = new Headers({ 'Content-Type': 'application/json' });
   private options = new RequestOptions({ headers: this.headers });
-  private users : Users;
+  private users: Users;
 
   constructor(public _http: Http) { }
 
   getUsers() {
-    return this._http.get(this.baseUrl + '/users/list-users', this.options).map((response: Response) => response.json()).catch(this.errorHandler);
+    return this._http.get(this.baseUrl + '/users/list-users', this.options).pipe(map((response: Response) => response.json().catchError(this.errorHandler)));
   }
 
   getUser(idUsers: Number) {
-    return this._http.get(this.baseUrl + '/users/list-id-users' + idUsers, this.options).map((response: Response) => response.json())
-      .catch(this.errorHandler);
+    return this._http.get(this.baseUrl + '/users/list-id-users' + idUsers, this.options).pipe(map((response: Response) => response.json().catchError(this.errorHandler)));
   }
 
   deleteUsers(idUsers: Number) {
-    return this._http.delete(this.baseUrl + '/users/' + idUsers, this.options).map((response: Response) => response.json())
-      .catch(this.errorHandler);
+    return this._http.delete(this.baseUrl + '/users/' + idUsers, this.options).pipe(map((response: Response) => response.json().catchError(this.errorHandler)));
   }
 
   createUsers(users: Users) {
-    return this._http.post(this.baseUrl + '/users/create-users', JSON.stringify(users), this.options).map((response: Response) => response.json())
-      .catch(this.errorHandler);
+    return this._http.post(this.baseUrl + '/users/create-users', JSON.stringify(users), this.options).pipe(map((response: Response) => response.json().catchError(this.errorHandler)));
   }
 
   updateUsers(users: Users) {
-    return this._http.put(this.baseUrl + '/users/update-users', JSON.stringify(users), this.options).map((response: Response) => response.json())
-      .catch(this.errorHandler);
+    return this._http.put(this.baseUrl + '/users/update-users', JSON.stringify(users), this.options).pipe(map((response: Response) => response.json().catchError(this.errorHandler)));
   }
 
   errorHandler(error: Response) {
